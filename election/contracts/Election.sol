68dffb9881458b107879dccdec888f1463e5d8cc
laihuiqi <huiiqiii22@gmail.com>
2023-08-24 20:44:48 +0800

    Update dapp - `election`.

---
+++ election/contracts/Election.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint public candidateCount;
+
+    event votedEvent (uint indexed _candidateId);
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+    } 
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidateCount);
+        
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+        emit votedEvent(_candidateId);
+    }
+}
---
+++ election/src/js/app.js
@@ -0,0 +1,111 @@
+App = {
+    web3Provider: null,
+    contracts: {},
+    account: '0x0',
+
+    init: function() {
+        return App.initWeb3();
+    },
+
+    initWeb3: function() {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            web3 = new Web3(App.currentProvider);
+        }
+
+        return App.initContract();
+    },
+
+    initContract: function() {
+        $.getJSON("Election.json", function(election) {
+            App.contracts.Election = TruffleContract(election);
+            App.contracts.Election.setProvider(App.web3Provider);
+            App.listenForEvents();
+            return App.render();
+        })
+    },
+
+    render: function() {
+        var electionInstance;
+        var loader = $("#loader");
+        var content = $("#content");
+
+        loader.show();
+        content.hide();
+
+        web3.eth.getCoinbase(function(err, account) {
+            if (err === null) {
+                App.account = account;
+                $("#accountAddress").html("Your Account: " + account);
+            }
+        });
+
+        App.contracts.Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            return electionInstance.candidatesCount();
+        }).then(function(candidatesCount) {
+            var candidatesResults = $("#candidatesResults");
+            candidatesResults.empty();
+
+            var candidatesSelect = $("#candidatesSelect");
+            candidatesSelect.empty();
+
+            for (var i = 1; i <= candidatesCount; i++) {
+                electionInstance.candidate(i).then(function(candidate) {
+                    var id = candidate[0];
+                    var name = candidate[1];
+                    var voteCount = candidate[2];
+
+                    var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+                    candidatesResults.append(candidateTemplate);
+
+                    var candidateOption = "<option value='" + id + "' >" + name + "</option>"
+                    candidatesSelect.append(candidateOption);
+
+                });
+            }
+            return electionInstance.voters(App.account);
+        }).then(function(hasVoted) {
+            if (hasVoted) {
+                $('form').hide();
+            }
+            loader.hide();
+            content.show();
+        }).catch(function(err) {
+            console.warn(err);
+        });
+    },
+
+    castVote: function() {
+        var candidateId = $('#candidatesSelect').val();
+        App.contracts.Election.deployed().then(function(instance) {
+            return instance.vote(candidateId, { from: App.account });
+        }).then(function(result) {
+            $("#content").hide();
+            $("#loader").show();
+        }).catch(function(err) {
+            console.error(err);
+        })
+    },
+
+    listenForEvents: function() {
+        App.contacts.Election.deployed().then(function(instance) {
+            return instance.votedEvent({}, {
+                fromBlock: 0,
+                toBlock: 'latest'
+            }).watch(function(err, event) {
+                console.log("event triggered", event);
+                App.render();
+            });
+        });
+    }
+};
+
+$(function() {
+    $(window).load(function() {
+        App.init();
+    });
+});
